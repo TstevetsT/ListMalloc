86f3a4577a0669890ba140cdc51d7e3a5f947612
TstevetsT <tstevetst@hushmail.com>
2014-03-23 15:44:57 -0700

    Update test_malloc_list.c

--- test_malloc_list.c
+++ test_malloc_list.c
@@ -30,30 +30,51 @@ struct rec
 		
 
 
-	void * ptr[10];
-	int toop;
-	int i;
-	int s;
-	int u=0;
-		for (i =0; i<10; i++)
+void * ptr[401];
+int toop;
+int i;
+int s;
+int u=0;
+int range=2000;
+int max=50;
+		for (i =1; i<max; i++)
 		{
-			s = i*87;
+			s = rand() % range+1;
 			ptr[i] = l_malloc(s);
-	    		printf("Pointer: %p Size: %i\n", ptr[i], s);
-			if (i>0)
+	    		printf("Pointer: %p Size: %i", ptr[i], s);
+			if (i>1)
 			{
 				toop=ptr[i]-ptr[i-1];
 				u=s+u;
-				printf("Diff: %i TotalUsed: %i\n", toop, u);
+				printf("   Byte Diff: %i TotalBytesUsed: %i\n", toop, u);
 			}
-		}		
-		for (i =0; i<10; i++)
+			else printf("\n");
+		}	
+
+		int t;	
+		for (i =0; i<(max/2); i++)
+		{
+			t = rand() % max+1;
+		        l_free(ptr[t]);
+			printf("%p has been freed\n", ptr[t]);
+		}
+
+		for (i =max+1; i<max*2; i++)
+		{
+			s = rand() % range+1;
+			ptr[i] = l_malloc(s);
+	    		printf("Pointer: %p Size: %i\n  ", ptr[i], s);
+		}
+
+		for (i =1; i<max*2; i++)
 		{	
     			l_free(ptr[i]);
+			//printf("%p have been freed\n",ptr[i]);
+			ptr[i]=0;
 		}
-   		return 0;
+		asm("test:);
     		
-   	int a,n;
+   		int a,n;
 		int * ptr_data;
 
 		printf ("Enter amount: ");
